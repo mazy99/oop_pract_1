494e31292461ef0adf3d7121c12ed2f3725a9944
mazy99 <mazy.1999@mail.ru>
2025-09-14 16:58:40 +0300

    delete_file

--- tasks/iomixin.py
+++
@@ -1,7 +0,0 @@
-class IOMixin:
-    def read(self):
-        self.first = int(input("Введите числитель: "))
-        self.second = int(input("Введите знаменатель: "))
-
-    def write(self):
-        print(f"{self.first}/{self.second}")
