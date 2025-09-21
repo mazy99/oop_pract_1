4637f4af8c5b0c0e0485331d80221273fcc729c9
mazy99 <mazy.1999@mail.ru>
2025-09-21 19:06:42 +0300

    update frac file

--- tasks/fraction_part.py
+++ tasks/fraction_part.py
@@ -1,15 +1,16 @@
 from math import gcd as gsd
 
+
 class FractionPart:
-    def __init__(self,first=0,second=1):
+    def __init__(self, first: int = 0, second: int = 1):
         if second == 0:
             raise ValueError("Знаменатель не может быть 0")
-        
+
         self.__first = int(abs(first))
         self.__second = int(abs(second))
 
         self.normalize()
-    
+
     def read(self):
         self.__first = int(input("Введите числитель: "))
         self.__second = int(input("Введите знаменатель: "))
@@ -17,30 +18,30 @@ class FractionPart:
         if self.__second == 0:
             raise ValueError("Знаменатель не может быть 0")
 
-
     def display(self):
-        
         print(f"{self.__first}/{self.__second}")
         print(f"Целая часть дроби: {self.ipart()}")
-    
+
     def normalize(self):
         g = gsd(self.__first, self.__second)
         self.__first //= g
-        self.__second //= g 
-
+        self.__second //= g
 
     @property
     def first(self):
-                return self.__first
-
+        return self.__first
 
     @property
     def second(self):
-                return self.__second
+        return self.__second
 
-    
     def ipart(self):
         return self.__first // self.__second
 
-frac = FractionPart(8, 4)
-frac.display()
+
+if __name__ == "__main__":
+    frac = FractionPart(5, 2)
+    frac.display()
+
+    frac.read()
+    frac.display()
